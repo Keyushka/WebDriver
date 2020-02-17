74b1694155588fef2c5f5bd6134cbe310c60aa67
Irina <keyushka@gmail.com>
2020-02-17 23:09:01 +0200

    return format 050XXXXXXX

--- src/test/java/tests/HW_1.java
+++ src/test/java/tests/HW_1.java
@@ -11,10 +11,24 @@ public class HW_1 {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             WebDriver driver = new ChromeDriver();
             driver.get("http://iteaua-develop.demo.gns-it.com/about-itea/");
+
             Thread.sleep(5000);
             String text = driver.findElement(selectors[i]).getText();
             System.out.println(text);
+            text = text.substring(3);
+            text = text.replaceAll("\\(", "");
+            text = text.replaceAll("\\)", "");
+            text = text.replaceAll(" ", "");
+            System.out.println(text);
             driver.quit();
+
+            /*if (text.equals("+380 800212405") || text.equals("+380 800212410")) {
+                String textSlice = text.substring(3,4)+text.substring(5);
+                System.out.println(textSlice);
+            } else {
+                String textSlice = text.substring(5,8)+text.substring(10,13)+text.substring(14,16)+text.substring(17,19);
+                System.out.println(textSlice);
+            }*/
         }
     }
 }
